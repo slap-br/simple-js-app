35802836fab40901c9686aed55db94ab23aa7832
slap-br <silvio.pelegrin@hotmail.com>
2022-09-10 00:19:19 +0200

    Pokemon list added
    - meta added to index.html,
    - pokemonList added,
    - strings w/ 3 keys;

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello world');
+//Task 2.1 
+//Classic pokemons list
 
-let favoritefood = 'pizza';
-document.write(pizza);
+let pokemonList = [
+    {
+      name : 'Bulbasaur',
+      height : 7,
+      types : ['grass', 'poison']
+    },
+    {
+      name : 'Charmander',
+      height : 8,
+      types : ['fire']
+    },
+    {
+      name : 'Butterfree',
+      height : 10,
+      types : ['bug', 'flying']
+    },
+    {
+      name : 'Squirtle',
+      height : 6,
+      types : ['water']
+    }
+
+    {
+        name: 'Pidgeotto',
+        height: '5',
+        types: ['flying', 'normal']
+    },
+    {
+        name: 'Pikachu',
+        height '4',
+        types: 'electric',
+    }
+  ]
