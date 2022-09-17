cfff3d47cf50de6f2743ab5a617bb20de5e96767
slap-br <silvio.pelegrin@hotmail.com>
2022-09-17 12:36:42 +0200

    added forEach()
    -  Part 1: forEach() Loops;
    - Part 2: IIFE;
    - pokemonRepository;
    - getAll;
    - function;

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,91 @@
 //Task 2.1 
 //Classic pokemons list
+//Create a new var "PokemonRepository" and assign to IIFE
 
-let pokemonList = [
-    {
-      name : 'bulbasaur',
-      height : 7,
-      types : ['grass', 'poison']
-    },
-
-    {
-      name : 'charmander',
-      height : 8,
-      types : ['fire']
-    },
-    
-    {
-      name : 'butterfree',
-      height : 10,
-      types : ['bug', 'flying']
-    },
-
-    {
-      name : 'squirtle',
-      height : 6,
-      types : ['water']
-    },
-
-    {
-        name: 'pidgeotto',
-        height: 5,
-        types: ['flying', 'normal']
-    },
-
-    {
-        name: 'pikachu',
-        height: 4,
-        types: 'electric',
-    }
-  ]
-
-  // Height limit threshold
-  let threshold = 8
-
-  for (let i = 0; i < pokemonList.length; i++) {
-    // Interpolates the string
-    let text = `${pokemonList[i].name} (height: ${pokemonList[i].height}")`
-
-    // If the height of the pokemon is bigger than 8, then:
-    if (pokemonList[i].height > threshold) {
-      // Concatenate the text 
-      text = `${text} - Wow, that's big!`
-    }
-
-      let div = "<div class='poke'>" + text + "</div>"
-      let lineBreak = "<br/>"
-
-    document.write(div, lineBreak)
+let pokemonRepository = (function() {
+  let pokemonList = [ //array with pokemons
+      {
+        name : 'bulbasaur',
+        height : 7,
+        types : ['grass', 'poison']
+      },
+  
+      {
+        name : 'charmander',
+        height : 8,
+        types : ['fire']
+      },
+      
+      {
+        name : 'butterfree',
+        height : 10,
+        types : ['bug', 'flying']
+      },
+  
+      {
+        name : 'squirtle',
+        height : 6,
+        types : ['water']
+      },
+  
+      {
+          name: 'pidgeotto',
+          height: 5,
+          types: ['flying', 'normal']
+      },
+  
+      {
+          name: 'pikachu',
+          height: 4,
+          types: 'electric',
+      }
+   ]
+  
+  // add item - pokemonList.push()
+  function add(item) {
+    return pokemonList.push(item);
   }
+  
+  function getAll() {
+    return pokemonList;
+  };
+
+  function heightGreaterThan (height) {
+    let pokemons = pokemonList.filter((p) => { return p.height > height });
+    return pokemons;
+  }
+  
+  
+   return {
+      getAll : getAll,
+      add : add,
+      heightGreaterThan : heightGreaterThan
+    };
+  
+  })()
+  
+  //adding a new pokemon to rep.
+  pokemonRepository.add({
+    name: 'Krabby', height: 0.4, types : 'water'});
+  
+    let pokemonList = pokemonRepository.getAll();
+    console.log(pokemonList);
+  
+  // Height limit threshold
+  const threshold = 8;
+  
+  pokemonList.forEach((pokemon) => {
+    // Interpolates the string
+    let pokemonDescription = `${pokemon.name} (height: ${pokemon.height}")`;
+    if (pokemon.height > threshold) {
+      // Concatenate the text 
+      pokemonDescription = `${pokemonDescription} - Wow, that's big!`;
+    }
+
+    let div = "<div class='poke'>" + pokemonDescription + "</div>";
+
+  document.write(div);
+    });
+  
+  
+  
